6ffa95d6d600edc7ffdb24e7b4955a79ed338eb2
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-10-18 21:11:20 +0300

    4. Переопределение [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,14 @@
+package ru.job4j.inheritance;
+
+import java.util.Scanner;
+
+public class JSONReport extends TextReport {
+
+    public String generate(String name, String body) {
+
+        return "{" + System.lineSeparator() + "\""
+                + name + "\"" + " : \"name\"" + System.lineSeparator()
+                + "\"" + body + "\"" + " : \"body\"" + System.lineSeparator() + "}";
+    }
+
+}
