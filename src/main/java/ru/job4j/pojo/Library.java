82284c6d3461120a816050cdcf5a084d06e77236
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-10-19 20:42:20 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,40 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book cleanCode = new Book("Clean code", 300);
+        Book philJava = new Book("Phylosofy Java", 254);
+        Book javaRukovodstvo = new Book("Java8 Rukovodstvo", 320);
+        Book headFirst = new Book("Head First", 300);
+        Book[] books = new Book[4];
+
+        books[0] = cleanCode;
+        books[1] = philJava;
+        books[2] = javaRukovodstvo;
+        books[3] = headFirst;
+
+        printBook(books);
+
+            Book temp = books[3];
+            books[3] = books[0];
+            books[0] = temp;
+
+          printBook(books);
+
+        for (int index = 0; index < books.length; index++) {
+            if (books[index].getName().equals("Clean code")) {
+                System.out.println("Название " + books[index].getName()
+                        + " количество страниц " + books[index].getCountPage());
+            }
+        }
+        }
+
+        public static void printBook(Book[] books) {
+            for (int index = 0; index < books.length; index++) {
+                System.out.println("Название " + books[index].getName()
+                        + " количество страниц " + books[index].getCountPage());
+            }
+            System.out.println();
+        }
+    }
+
