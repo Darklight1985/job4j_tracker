c4eaa95cd87a30331d1601f3c5da12aa0feb71a9
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-10-20 22:20:00 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -14,12 +16,37 @@ public class Tracker {
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = items[index];
                 break;
             }
         }
+        return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] withoutNull = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] != null) {
+                withoutNull[size] = items[index];
+                size++;
+            }
+        }
+        withoutNull = Arrays.copyOf(withoutNull, size);
+        return withoutNull;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] != null && key.equals(items[index].getName())) {
+                rsl[size] = items[index];
+                size++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, size);
         return rsl;
     }
 }
