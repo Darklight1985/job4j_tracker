226b00c8df303f329f688e688391f40406eaa514
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-10-31 23:08:45 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,15 +5,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.Objects.nonNull;
-
+/**
+ * Класс описыывает модель банковской системы
+ */
 public class BankService {
+    /**
+     * Поле Map хранит клиентов и список их счетов
+     */
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * метод добавляет клиентов в поле users и создает пустой список счетов если клиента нет в базе
+     * @param user - клиент банка
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * добавляет новый счет клиенту
+     * @param passport - паспортные данные
+     * @param account - номер аккаунта
+     */
     public void addAccount(String passport, Account account) {
              User user = findByPassport(passport);
         if (user != null) {
@@ -24,6 +37,11 @@ public class BankService {
         }
     }
 
+    /**
+     * ищет клиента по паспортным данным
+     * @param passport - паспортные данные
+     * @return возвращает клиента или null если таквоого нет в базе
+     */
     public User findByPassport(String passport) {
         User rsl = null;
         for (User key: users.keySet()) {
@@ -35,6 +53,12 @@ public class BankService {
         return rsl;
     }
 
+    /**
+     * ищет номер счета по реквизитам
+     * @param passport - паспортные данные клиента
+     * @param requisite реквизиты счета клиента
+     * @return возвращает либо номер счета либо null если такого нет в базе
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -48,6 +72,17 @@ public class BankService {
         return null;
     }
 
+    /**
+     * перечисляет деньги со счета одного клиента на счет другого
+     * @param srcPassport - паспорт клиент отправляющего деньги
+     * @param srcRequisite - реквзиты счета клиента отправляющего деньги
+     * @param destPassport - паспортные данные клиента получающего деньги
+     * @param destRequisite - реквизиты счета клиента получающего деньги
+     * @param amount - величина пересляемых средств
+     * @return возвращает true если операция проходит успешно
+     * либо false если нету счета у одного из клиентов или
+     * денег на счету клиента недостаточно для перечисления
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
