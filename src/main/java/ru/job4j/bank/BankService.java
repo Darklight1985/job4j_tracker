8811f4add4eb7d777fbc4df859383c6a45c34eb3
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-10-31 18:23:04 +0300

    3. Банковские переводы [#10038 #222435] исправил

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -11,18 +11,17 @@ public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-        if (!users.containsKey(user)) {
-            users.put(user, new ArrayList<>());
-        }
+        users.putIfAbsent(user, new ArrayList<>());
     }
 
     public void addAccount(String passport, Account account) {
              User user = findByPassport(passport);
-              List<Account> list;
-              list = users.get(user);
+        if (user != null) {
+            List<Account> list = users.get(user);
             if (!list.contains(account)) {
                 users.get(user).add(account);
             }
+        }
     }
 
     public User findByPassport(String passport) {
@@ -37,44 +36,28 @@ public class BankService {
     }
 
     public Account findByRequisite(String passport, String requisite) {
-        User user;
-        Account rsl = null;
-        List<Account> list;
-            user = findByPassport(passport);
-             if (nonNull(user)) {
-                 list = users.get(user);
-                 for (Account account : list) {
-                     if (account.getRequisite().equals(requisite)) {
-                         rsl = account;
-                     }
-                 }
-             }
-        return rsl;
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> list = users.get(user);
+            for (Account account : list) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
-        User userSource = findByPassport(srcPassport);
         Account source = findByRequisite(srcPassport, srcRequisite);
-        User userDest = findByPassport(destPassport);
         Account dest = findByRequisite(destPassport, destRequisite);
-         List<Account> listDest;
-        List<Account> listSource;
-
         double balanceSource = source.getBalance();
-        if (balanceSource >= amount) {
+        if (source != null && dest != null && source.getBalance() >= amount) {
             double balanceDest = dest.getBalance();
-            listDest = users.get(userDest);
-            listSource = users.get(userSource);
                 dest.setBalance(balanceDest + amount);
                 source.setBalance(balanceSource - amount);
-
-                listDest.set(listDest.indexOf(dest), dest);
-                listSource.set(listSource.indexOf(source), source);
-
-                users.putIfAbsent(userDest, listDest);
-                users.putIfAbsent(userSource, listSource);
                 rsl = true;
             }
         return rsl;
