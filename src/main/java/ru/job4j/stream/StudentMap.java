04dfe3188a0f3b7d80b4021ee0df3940116cd9e6
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-11-05 17:24:30 +0300

    4. Преобразование List в Map. [#110226]

---
+++ src/main/java/ru/job4j/stream/StudentMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class StudentMap {
+    public Map<String, Student> collect(List<Student> students) {
+             return students.stream()
+                     .distinct()
+                     .collect(Collectors
+                             .toMap(e -> e.getSurname(), e -> e));
+    }
+}
---
+++ src/test/java/ru/job4j/stream/StudentMapTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class StudentMapTest {
+
+    @Test
+    public void whenCorrecctMap() {
+        List<Student> students = List.of(
+                new Student(10, "Surname1"),
+                new Student(40, "Surname4"),
+                new Student(50, "Surname5"),
+                new Student(50, "Surname5"),
+                new Student(70, "Surname7"),
+                new Student(90, "Surname9")
+        );
+       StudentMap studentMap = new StudentMap();
+        Map<String, Student> rsl = studentMap.collect(students);
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("Surname9", new Student(90, "Surname9"));
+        expected.put("Surname1", new Student(10, "Surname1"));
+        expected.put("Surname4", new Student(40, "Surname4"));
+        expected.put("Surname5", new Student(50, "Surname5"));
+        expected.put("Surname7", new Student(70, "Surname7"));
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenCorrectMapTwo() {
+        List<Student> students = List.of(
+                new Student(10, "Ivanov"),
+                new Student(40, "Pushkin"),
+                new Student(50, "Pushkin"),
+                new Student(50, "Dulin"),
+                new Student(70, "Dulin"),
+                new Student(90, "Jmurkin")
+        );
+        StudentMap studentMap = new StudentMap();
+        Map<String, Student> rsl = studentMap.collect(students);
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("Ivanov", new Student(10, "Ivanov"));
+        expected.put("Pushkin", new Student(50, "Pushkin"));
+        expected.put("Dulin", new Student(70, "Dulin"));
+        expected.put("Jmurkin", new Student(90, "Jmurkin"));
+        rsl.entrySet().forEach(s -> System.out.println(s));
+        assertThat(rsl, is(expected));
+    }
+}
