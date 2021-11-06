c70612c30d94cb6fb6f5e2e97d7ccabf96e1c2f0
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-11-06 12:04:13 +0300

    Преобразование в единый список

---
+++ src/main/java/ru/job4j/stream/MatrixStreamList.java
@@ -0,0 +1,17 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MatrixStreamList {
+    public List<List<Integer>> transform(Integer[][] matrix) {
+        List<List<Integer>> list = Stream.of(matrix)
+                .map(Arrays::asList)
+                .collect(Collectors.toList());
+
+        System.out.println(list.stream().flatMap(e -> e.stream()).collect(Collectors.toList()));
+        return list;
+    }
+}
