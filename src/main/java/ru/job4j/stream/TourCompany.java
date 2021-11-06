536fbdaf0580bb2a840c5dae36c4bd12177fde77
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-11-06 10:31:18 +0300

    2. Список адресов [#110062 #224881] исправил

--- src/main/java/ru/job4j/stream/TourCompany.java
+++ src/main/java/ru/job4j/stream/TourCompany.java
@@ -1,16 +1,16 @@
 package ru.job4j.stream;
 
+
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class TourCompany {
     public List<Address> collect(List<Profile> profiles) {
-       return profiles.stream()
+        return profiles.stream()
                .map(Profile::getAddress)
-               .sorted(Address::compareTo)
+               .sorted(Comparator.comparing(Address::getCity))
                .distinct()
                .collect(Collectors.toList());
-
     }
 }
