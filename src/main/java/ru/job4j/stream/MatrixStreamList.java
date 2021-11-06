2295a57aa70ec0842ef598d73c630aeacb7ddaaa
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-11-06 22:11:39 +0300

    5. Преобразование матрицы чисел в список чисел. [#110228 #225000] new 2

--- src/main/java/ru/job4j/stream/MatrixStreamList.java
+++ src/main/java/ru/job4j/stream/MatrixStreamList.java
@@ -6,12 +6,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MatrixStreamList {
-    public List<List<Integer>> transform(Integer[][] matrix) {
+    public List<Integer> transform(Integer[][] matrix) {
         List<List<Integer>> list = Stream.of(matrix)
                 .map(Arrays::asList)
                 .collect(Collectors.toList());
 
-        System.out.println(list.stream().flatMap(e -> e.stream()).collect(Collectors.toList()));
-        return list;
+       return list.stream().flatMap(e -> e.stream()).collect(Collectors.toList());
     }
 }
---
+++ src/test/java/ru/job4j/stream/MatrixStreamListTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class MatrixStreamListTest {
+
+    @Test
+    public void transform() {
+        Integer[][] matrix = {{1, 2}, {3, 4}};
+        List<Integer> expected = List.of(1, 2, 3, 4);
+        MatrixStreamList matrixStream = new MatrixStreamList();
+        List<Integer> rsl = matrixStream.transform(matrix);
+        assertThat(expected, is(rsl));
+    }
+
+    @Test
+    public void transformHard() {
+        Integer[][] matrix = {{1, 2}, {3, 4}, {5, 6}};
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6);
+        MatrixStreamList matrixStream = new MatrixStreamList();
+        List<Integer> rsl = matrixStream.transform(matrix);
+        assertThat(expected, is(rsl));
+    }
+}
